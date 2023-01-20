f674adcc61b3b48e0218ac0edac834a75b814d0b
Krage56 <ivan.proskuryakov.2001@yandex.ru>
2023-01-20 12:05:49 +0300

    fix: в комментарии добавлена грамматика

--- main.cpp
+++ main.cpp
@@ -8,7 +8,15 @@
 
 //Сюда записывается считываемая лексема
 char buf[buf_size];
-
+/*
+<statement> ::= <st_switch>
+<st_switch> ::= SWITCH(INT)<body_switch>
+<body_switch> ::= {<case_arr>}
+<case_arr> ::= <st_case><case_arr> | <st_default> | e
+<st_case> ::= CASE INT:<payload>
+<payload> ::= <statement><payload> | print(INT); <payload> | BREAK; <payload> | e
+<st_default> ::= DEFAULT:<payload>
+*/
 enum TOKENS{
     SWITCH, //0
     CASE,//1
